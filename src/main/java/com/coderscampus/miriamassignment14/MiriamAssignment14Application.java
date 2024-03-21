dd1cd0e7e5fdfd600f5d6dcf4a580aea95a13b47
MiriamLaw <mimsical999@yahoo.com>
2024-03-20 19:35:30 -0700

    Initial commit

---
+++ src/main/java/com/coderscampus/miriamassignment14/MiriamAssignment14Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.miriamassignment14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiriamAssignment14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiriamAssignment14Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderscampus/miriamassignment14/MiriamAssignment14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.miriamassignment14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiriamAssignment14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
