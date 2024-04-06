b368330ab567a1572ea8a633130c8654b02973cf
MiriamLaw <mimsical999@yahoo.com>
2024-04-06 11:36:07 -0700

    js continuation

--- src/main/java/com/coderscampus/miriamassignment14/web/ChannelController.java
+++ src/main/java/com/coderscampus/miriamassignment14/web/ChannelController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.coderscampus.miriamassignment14.domain.Channel;
@@ -43,6 +45,16 @@ public class ChannelController {
 	    List<Channel> channels = channelService.findAll();
 	    model.addAttribute("channels", channels);
 	    return "channels";
+	}
+	
+	@PostMapping("/channels/create")public String createChannel(@RequestParam String channelName, RedirectAtributes redirectAttributes) {
+		Channel newChannel = new Channel();
+		newChannel.setName(channelName);
+		Channel savedChannel = channelService.save(newChannel);
+		
+		redirectAttributes.addFlashAttribute("successMessage", "Channel '" + channelName + "' created successfully");
+		
+		return "redirect:/channels";
 	}
 
 	
