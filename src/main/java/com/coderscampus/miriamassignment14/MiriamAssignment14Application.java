5a4b6365336e336cc69a57c41e6dd7fdd6cc5abd
MiriamLaw <mimsical999@yahoo.com>
2024-04-17 19:58:37 -0700

    Still troubleshooting

--- src/main/java/com/coderscampus/miriamassignment14/MiriamAssignment14Application.java
+++ src/main/java/com/coderscampus/miriamassignment14/MiriamAssignment14Application.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MiriamAssignment14Application {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MiriamAssignment14Application.class, args);
-	}
 
+	}
 }
